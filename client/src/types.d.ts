b2b00e8e772d94bb96056bed0f6ae0801d4ac4cf
dennis ko <dennis0324@naver.com>
2023-09-08 22:10:05 +0900

    add: image file modules

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -3,3 +3,5 @@
 // whether you're running in development or production).
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
 declare const MAIN_WINDOW_VITE_NAME: string;
+declare module '*.jpg';
+declare module '*.png';
