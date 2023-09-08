659cf5072ef1e8cc8795840e84c52dbd0f57b26d
dennis ko <dennis0324@naver.com>
2023-09-08 21:58:28 +0900

    add: game resource files

---
+++ client/src/source/background/index.ts
@@ -0,0 +1,11 @@
+import background from './1.png';
+import background_cloud_far from './2.png'
+import background_cloud_close from './3.png'
+import background_cloud_closest from './4.png'
+
+export const backgrounds = [
+  background,
+  background_cloud_far,
+  background_cloud_close,
+  background_cloud_closest
+]
---
+++ client/src/source/horse/index.ts
@@ -0,0 +1,8 @@
+import block1 from './block1.png'
+import block2 from './block2.png'
+import block3 from './block3.png'
+import block4 from './block4.png'
+import block5 from './block5.png'
+import block6 from './block6.png'
+
+export const blocks = [ block1,block2,block3,block4,block5,block6 ]
